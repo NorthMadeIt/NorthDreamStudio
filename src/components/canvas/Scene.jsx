f91293b0f8e391f5b88fd1f5948e88a511940963
NorthMadeIt <therealteedeshogun@gmail.com>
2026-08-01 08:24:00 -0400

    Add Scene component with Three.js canvas

---
+++ src/components/canvas/Scene.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import FloatingObject from "./FloatingObject";
+
+export default function Scene() {
+  return (
+    <div className="fixed inset-0 pointer-events-auto z-0">
+      <Canvas
+        camera={{ position: [0, 0, 8], fov: 45 }}
+        style={{ background: "#e5e7eb" }}
+      >
+        <ambientLight intensity={1.2} />
+        <directionalLight position={[10, 10, 10]} intensity={1.5} />
+        
+        {/* Scattered 3D Curiosities */}
+        <FloatingObject
+          position={[-2.5, 1.2, 0]}
+          geometryType="torus"
+          scale={0.9}
+          color="#111111"
+        />
+        <FloatingObject
+          position={[2.8, -0.8, -1]}
+          geometryType="icosahedron"
+          scale={1.1}
+          color="#dedede"
+        />
+        <FloatingObject
+          position={[0, -2, 1]}
+          geometryType="box"
+          scale={0.7}
+          color="#e2ff70"
+        />
+      </Canvas>
+    </div>
+  );
+}
